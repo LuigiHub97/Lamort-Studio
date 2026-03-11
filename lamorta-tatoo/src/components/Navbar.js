705f8a218136744f958f9b643269445f52882a84
Luigi Scaglione <luigiscaglionejob2@outlook.com>
2026-03-11 13:47:54 +0000

    adicionei estilos

---
+++ lamorta-tatoo/src/components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>LaMort</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Galeria</li>
+        <li>Contato</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
