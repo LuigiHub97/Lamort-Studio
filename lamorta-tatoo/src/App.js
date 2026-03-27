afa9bd975bc5a0e773331d144e727334f56c746f
Luigi Scaglione <luigiscaglionejob2@outlook.com>
2026-03-27 15:24:13 +0000

    mudança de layout e instalação de react icons

--- lamorta-tatoo/src/App.js
+++ lamorta-tatoo/src/App.js
@@ -1,17 +1,53 @@
 import "./App.css";
 import Galeria from "./galeria";
+import { FaInstagram, FaFacebookF } from "react-icons/fa";
+import { SiThreads } from "react-icons/si";
 
 function App() {
   return (
     <div className="App">
-      {/* Hero */}
+      {/* HERO */}
       <div className="hero">
-        <img src="/Logo.jpg" alt="Logo" />
-        <h1>Lamort Tattoo Studio</h1>
-        <p>Especialista em blackwork</p>
+        <div className="hero-content">
+          <h1>LAMORT</h1>
+          <p>TATTOO STUDIO</p>
+
+          <button>
+            <span>AGENDAR SESSÃO</span>
+          </button>
+
+          {/* 👇 ADICIONA ISSO */}
+          <div className="hero-social">
+            <a
+              href="https://www.instagram.com/lamort.ink/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.threads.com/@lamort.ink"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Threads
+            </a>
+            <a
+              href="https://facebook.com/lamort.studio"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+          </div>
+          <button>História</button>
+          <button>Tatuador</button>
+          <button>Studio</button>
+          {/* 👆 SÓ ISSO */}
+        </div>
       </div>
 
-      {/* Galeria */}
+      {/* GALERIA */}
       <Galeria />
     </div>
   );
